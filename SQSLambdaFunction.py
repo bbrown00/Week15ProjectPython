6cc3b7d425ac6dc0340db28bf1f525b217f5ec60
bbrown00 <116729775+bbrown00@users.noreply.github.com>
2023-01-19 12:18:41 -0500

    Update SQSLambdaFunction.py

--- SQSLambdaFunction.py
+++ SQSLambdaFunction.py
@@ -7,3 +7,27 @@ sqs = boto3.resource('sqs')
 queue = sqs.create_queue(QueueName='Week15Messages')
 print(queue.url)
 print(queue.attributes)
+
+
+#Created lambda function and message in AWS console
+
+import json 
+import boto3 
+import datetime as datetime
+
+#lambda function with datetime
+
+def lambda_handler(event, context): 
+    current_datetime = datetime.datetime.now()
+    string_date = current_datetime.strftime("%A, %B %d, %Y %H:%M:%S")
+    
+#SQS Fuction with URL link
+
+    sqs = boto3.client('sqs')
+    response = sqs.send_message(
+    QueueUrl ="https://sqs.us-east-2.amazonaws.com/135662092194/Week15Messages",
+    MessageBody=string_date)
+    return {
+    'statusCode': 200,
+    'body': json.dumps('Lambda Function is for SQS!')
+}
